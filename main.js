088b9bfb4fe1de3b15ee6bacdd3d9bc02756e15a
ImmaHarry <haritkhera@gmail.com>
2022-06-06 00:29:29 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        height: 1920,
+        width: 1080,
+        webPreferences: {
+            preload: path.join(__dirname, 'scripts/preload.js'),
+            nodeIntegration: true
+        },
+        icon: path.join(__dirname, 'app-icon.png')
+    })
+    
+    win.loadFile('Webpage Files/landing-page.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
